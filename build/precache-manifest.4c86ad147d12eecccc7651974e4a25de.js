5bf4b35201c4513e39df206add4d60354205fea0
jmauwerb <jeroen.werbrouck@ugent.be>
2021-10-06 17:04:44 +0200

    add build

---
+++ build/precache-manifest.4c86ad147d12eecccc7651974e4a25de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a5f107b0127c8b65f70f674f56eb08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a9c100db526ad7ab450",
+    "url": "/static/css/2.16ac8c00.chunk.css"
+  },
+  {
+    "revision": "66ae4825b8c0f12d84b8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5a9c100db526ad7ab450",
+    "url": "/static/js/2.17ea13f7.chunk.js"
+  },
+  {
+    "revision": "d1189fc48e4b0932e9d9d5458eb92736",
+    "url": "/static/js/2.17ea13f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ae4825b8c0f12d84b8",
+    "url": "/static/js/main.5a5b7aea.chunk.js"
+  },
+  {
+    "revision": "7c8c9df327b3169830d0",
+    "url": "/static/js/runtime-main.f2ead44a.js"
+  }
+]);
